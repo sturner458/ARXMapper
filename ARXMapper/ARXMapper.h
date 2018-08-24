f25d5da636e5edc2c9d05a076b9a4970e8a592eb
Simon Turner <sturner@macbook-air.local>
2018-08-24 09:37:18 +0100

    Initial Commit

---
+++ ARXMapper/ARXMapper.h
@@ -0,0 +1,13 @@
+//
+//  ARXMapper.h
+//  ARXMapper
+//
+//  Created by Simon Turner on 24/08/2018.
+//  Copyright © 2018 Simon Turner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ARXMapper : NSObject
+
+@end
---
+++ ARXMapper/ARXMapper.m
@@ -0,0 +1,13 @@
+//
+//  ARXMapper.m
+//  ARXMapper
+//
+//  Created by Simon Turner on 24/08/2018.
+//  Copyright © 2018 Simon Turner. All rights reserved.
+//
+
+#import "ARXMapper.h"
+
+@implementation ARXMapper
+
+@end
